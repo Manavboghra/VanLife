6649cf02875560437b4f742e79cb4335998928ad
Manav <manavboghraa12@gmail.com>
2025-09-04 12:21:32 +0530

    changes made for rollback

--- src/utils/RequireAuth.jsx
+++ src/utils/RequireAuth.jsx
@@ -1,10 +1,11 @@
 import { redirect } from "react-router-dom";
-
-export default async function requireAuth(pathname) {
+export default async function requireAuth(request) {
   const isLoggedIn = localStorage.getItem("loggedin");
+  
+  const pathname = new URL(request.url).pathname;
 
   if (!isLoggedIn) {
-    const redirectUrl = `/login?message=You must log in first.&redirectTo=${encodeURIComponent(pathname)}` ;
+    const redirectUrl = `/login?message=You must log in first.&redirectTo=${encodeURIComponent(pathname)}`;
     
     throw redirect(redirectUrl);
   }
